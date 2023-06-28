775d83452a4be5e11e27d090dc12731636485370
aalexebr <alex.ebr@icloud.com>
2023-06-28 14:53:50 +0200

    EDIT milestone 2 almost done

--- js/main.js
+++ js/main.js
@@ -81,13 +81,13 @@ for(let i =0; i < posts.length; i++){
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" href="#/" data-postid="1">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-${i+1}" class="js-likes-counter">80</b> persone
+                Piace a <b id="like-counter-${i}" class="js-likes-counter" type="button">${getRng(1,100)}</b> persone
             </div>
         </div> 
     </div>            
@@ -95,3 +95,27 @@ for(let i =0; i < posts.length; i++){
 
 document.querySelector('.posts-list').innerHTML+= singlePost
 }
+let likedPosts = []
+const allBtns = document.querySelectorAll('.like-button');
+
+// console.log('')
+for(let i =0; i < allBtns.length; i++){
+    const likeNumber = parseInt(document.getElementById(`like-counter-${i}`).innerHTML)
+    
+    allBtns[i].addEventListener('click', function(){
+        allBtns[i].classList.add('like-button--liked')
+        
+        document.getElementById(`like-counter-${i}`).innerHTML= likeNumber+1
+        likedPosts.push(i)
+        console.log(likedPosts)
+        
+    })
+}
+console.log(likedPosts)
+
+
+
+// FUNCTIONS
+function getRng(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
